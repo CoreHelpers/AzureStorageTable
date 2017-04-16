f9e2ef1a1752e38250f2f330774c7a38d5a13ece
Dirk Eisenberg <dirk.eisenberg@gmail.com>
2017-04-17 01:36:45 +0200

    added AssemblyInfo.cs

---
+++ CoreHelpers.WindowsAzure.Storage.Table/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: AssemblyCompany("CoreHelpers.WindowsAzure.Storage.Table")]
+[assembly: AssemblyProduct("CoreHelpers.WindowsAzure.Storage.Table")]
+[assembly: AssemblyTitle("CoreHelpers.WindowsAzure.Storage.Table")]
+
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+
+#if (DEBUG)
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
