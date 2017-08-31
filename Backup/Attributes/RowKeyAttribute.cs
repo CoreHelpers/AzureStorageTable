82fb09fb3ff2ab24a0a11e54a3c4ef2a350ff032
Peter <peter@ecomerc.com>
2017-08-31 20:46:20 +0200

    removed dumb backup folder, and fixed error in the nuspec file

--- Backup/Attributes/RowKeyAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace CoreHelpers.WindowsAzure.Storage.Table.Attributes
-{
-	[AttributeUsage(AttributeTargets.Property)]
-    public class RowKeyAttribute : Attribute
-    {        
-    }
-}
