fd813c7f29b54a12fd523fb71e10a133f4bea85e
dvolper <github@david.vollmers.org>
2023-03-31 11:06:42 +0200

    Allow using query filters for non-string types (#25)
    * implement filter support for all known data types

--- CoreHelpers.WindowsAzure.Storage.Table.Tests/TestEnvironments/UnittestStorageEnvironment.cs
+++ CoreHelpers.WindowsAzure.Storage.Table.Tests/TestEnvironments/UnittestStorageEnvironment.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using CoreHelpers.WindowsAzure.Storage.Table.Tests.Contracts;
+﻿using CoreHelpers.WindowsAzure.Storage.Table.Tests.Contracts;
 
 namespace CoreHelpers.WindowsAzure.Storage.Table.Tests.TestEnvironments
 {
     public class UnittestStorageEnvironment : ITestEnvironment
-    {       
-        public string ConnectionString {
-            get {
-
+    {
+        public string ConnectionString
+        {
+            get
+            {
                 var connectionString = Environment.GetEnvironmentVariable("STORAGE");
-                if (!String.IsNullOrEmpty(connectionString))
+                if (!string.IsNullOrEmpty(connectionString))
                 {
                     Console.WriteLine("Using environment credentials");
                     return connectionString;
                 }
 
-                var filePath = Environment.ExpandEnvironmentVariables(Path.Combine("%HOME%", ".corehelpers.credentials.txt"));
+                var filePath = Environment.ExpandEnvironmentVariables(Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".corehelpers.credentials.txt"));
                 Console.WriteLine("Using filesystem credentials");
+
                 return File.ReadLines(filePath).First();
             }
         }
     }
 }
-
