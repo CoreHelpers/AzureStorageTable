f955f915f1a5e53e81ea790f658348bdcd08e1f9
Dirk Eisenberg <dirk.eisenberg@gmail.com>
2023-11-09 22:18:49 +0100

    Allow to use regex patten for table exclude check when doing a backup

---
+++ CoreHelpers.WindowsAzure.Storage.Table.Backup/Internal/TabledExcludeExpressionValidator.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace CoreHelpers.WindowsAzure.Storage.Table.Backup.Internal;
+
+internal class TabledExcludeExpressionValidator
+{
+    private List<string> _excludes = new List<string>();
+    public TabledExcludeExpressionValidator(string[]? excludes)
+    {
+        if (excludes != null)
+            _excludes = excludes.Select(e => e.ToLower()).ToList();
+    }
+
+    public bool IsTableExcluded(string tableName)
+    {
+        // verify if the tablename is in the exclude list
+        if (_excludes.Contains(tableName.ToLower()))
+            return true;
+        
+        // verify if the tablename matches a regex pattern
+        foreach (var exclude in _excludes)
+        { 
+            if (Regex.IsMatch(tableName, exclude, RegexOptions.IgnoreCase)) 
+                return true;
+        }
+        
+        // all the rest don't exclude
+        return false;
+    } 
+}
